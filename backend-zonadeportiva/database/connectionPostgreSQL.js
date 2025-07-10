7ce223c65e2eaa28e7c6cfe15d4379c5f27bdab6
Nicolas Gajardo <nicolasgajardo11@gmail.com>
2025-07-10 04:18:38 -0400

    cambio en la conexion de bd

--- backend-zonadeportiva/database/connectionPostgreSQL.js
+++ backend-zonadeportiva/database/connectionPostgreSQL.js
@@ -1,12 +1,9 @@
 import pg from 'pg';
 import dotenv from 'dotenv';
 
-dotenv.config(); 
+dotenv.config();
 
 export const pool = new pg.Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }, // necesario para Render
 });
